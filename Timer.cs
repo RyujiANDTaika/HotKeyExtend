7509b51af94a0787f8b133ae3b355e7f6b05c640
MrWang98 <w1968953844@163.com>
2020-06-01 14:56:50 +0800

    完善了Timer类，但是使用还有问题

--- Timer.cs
+++ Timer.cs
@@ -8,13 +8,51 @@ namespace HotkeyExtend
 {
     class Timer
     {
+        //定义Timer类变量
+        private static int interval = 1000;//时间间隔触发事件，单位ms
+        private System.Timers.Timer Mytimer = new System.Timers.Timer(interval);
+
+        //用于表示时间，单位s
+        private long TimeCount;
+        //表示此时计时器的状态
+        private bool status = false;
+
         public delegate void timerEndEventHandler();
         public timerEndEventHandler timerEnd;
 
         public void start()
         {
+            TimeCount = 3;
+            Mytimer.Start();
+            Mytimer.Elapsed += new System.Timers.ElapsedEventHandler(Mytimer_tick);
+            status = true;
+        }
+
+        //结束计时
+        public void stop()
+        {
+            Mytimer.Stop();
+            status = false;
+        }
+
+        //每经过时间间隔触发一次
+        private void Mytimer_tick(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            TimeCount--;//每经过时间间隔减1s
+            if (TimeCount == 0)
+            {
+                stop();//计时结束
+                timerEnd();//直接调用timerEnd()
+                start();//重新开始计时
+            }
 
         }
+        private void ShowTime(long TimeCount)
+        {
+            TimeSpan temp = new TimeSpan(0, 0, (int)(TimeCount));
+            Console.WriteLine(string.Format("{0:00}:{1:00}:{2:00}", temp.Hours, temp.Minutes, temp.Seconds));
+        }
+
 
         public bool hasEvent(timerEndEventHandler timerEndEvent)
         {
